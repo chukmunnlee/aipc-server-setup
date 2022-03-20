d8abfd26ddcba01debc91983018f52078ea9c82f
chukmunnlee <chukmunnlee@gmail.com>
2022-03-20 15:10:53 +0800

    added ansible to setup server

--- provider.tf
+++ provider.tf
@@ -10,6 +10,15 @@ terraform {
       version = "2.2.2"
     }
   }
+  backend s3 {
+    skip_credentials_validation = true
+    skip_region_validation = true
+    skip_metadata_api_check = true
+    endpoint = "https://sgp1.digitaloceanspaces.com"
+    region = "sgp1"
+    bucket = "bigbucket"
+    key = "states/terraform.tfstate"
+  }
 }
 
 provider digitalocean {
